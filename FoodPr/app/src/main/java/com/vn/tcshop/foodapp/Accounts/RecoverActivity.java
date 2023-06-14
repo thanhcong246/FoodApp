3f15b42f9b7c2a5e2f6d739bf8bf231432782554
thanhcong246 <nguyenthanhcong4869@gmail.com>
2023-06-14 15:41:08 +0700

    user

---
+++ FoodPr/app/src/main/java/com/vn/tcshop/foodapp/Accounts/RecoverActivity.java
@@ -0,0 +1,16 @@
+package com.vn.tcshop.foodapp.Accounts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.vn.tcshop.foodapp.R;
+
+public class RecoverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recover);
+    }
+}
